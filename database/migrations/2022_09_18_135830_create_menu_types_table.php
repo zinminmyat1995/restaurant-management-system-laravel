e062ebf0cfb619328804ab8b2aeb184c4e19b306
AungPyaeSoneHtunn <aungpyaesone1122000@gmail.com>
2023-01-07 21:05:11 +0630

    migration complete

---
+++ database/migrations/2022_09_18_135830_create_menu_types_table.php
@@ -0,0 +1,37 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+class CreateMenuTypesTable extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('menu_types', function (Blueprint $table) {
+            $table->id();
+            $table->integer('menu_type');
+            $table->string('name',255);
+            $table->softDeletes('deleted_at');
+            $table->integer('created_emp');
+            $table->integer('updated_emp');
+            $table->timestamp('created_at')->useCurrent();
+            $table->timestamp('updated_at')->useCurrent();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::dropIfExists('menu_types');
+    }
+}
---
+++ database/migrations/2023_01_07_135936_create_order_table.php
@@ -0,0 +1,42 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+class CreateOrderTable extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('order', function (Blueprint $table) {
+            $table->id();
+            $table->string('shop_code');
+            $table->integer('token_number');
+            $table->integer('table_number');
+            $table->integer('import_seat_count');
+            $table->integer('total_price');
+            $table->integer('paid');
+            $table->timestamp('deleted_at')->useCurrent();
+            $table->string('created_emp');
+            $table->string('updated_emp');
+            $table->timestamp('created_at')->useCurrent();
+            $table->timestamp('updated_at')->useCurrent();
+
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::dropIfExists('order');
+    }
+}
